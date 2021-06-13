55ea869c3343b7d059b6205fd718bfe2dedb24b1
Chathuru <chathurams.sa@gmail.com>
2021-06-13 13:47:26 +0530

    Initial commit

---
+++ python/main.py
@@ -0,0 +1,16 @@
+from prometheus_client import start_http_server, Gauge
+import random
+import time
+import psutil
+
+if __name__ == '__main__':
+
+   gauge = Gauge('system_process_count', 'System Process Count')
+   start_http_server(8000)
+
+   try:
+      while True:
+         gauge.set(len(psutil.pids()))
+         time.sleep(1)
+   except KeyboardInterrupt:
+      print("Stoping Prometheus Custom metric server")
